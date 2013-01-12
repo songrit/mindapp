c283175340b9679d98e3835a79afc5303b108d16
songrit <songrit@gmail.com>
2013-01-13 02:30:43 +0700

    rm application_helper.rb

--- lib/generators/mindapp/templates/app/helpers/application_helper.rb
+++
@@ -1,6 +0,0 @@
-# -*- encoding : utf-8 -*-
-# Methods added to this helper will be available to all templates in the application.
-module ApplicationHelper
-  # fix problem when run in production environment
-  include Elocal
-end
