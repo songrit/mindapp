410f7d2bf1bb70e0a95073f078b00f869097fa65
songrit <songrit@gmail.com>
2013-01-24 08:58:45 +0700

    edit user role

--- lib/generators/mindapp/templates/app/controllers/admins_controller.rb
+++ lib/generators/mindapp/templates/app/controllers/admins_controller.rb
@@ -1,2 +1,6 @@
 class AdminsController < ApplicationController
+  def update_role
+    user = User.find_by :code=> $xvars["select_user"]["code"]
+    user.update_attribute :role, $xvars["edit_role"]["role"]
+  end
 end
