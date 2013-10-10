917c2332c5d22ca702511a0c1db4cba0c736b2ad
songrit <songrit@gmail.com>
2013-10-10 13:02:05 +0700

    fix request.env

--- lib/mindapp/helpers.rb
+++ lib/mindapp/helpers.rb
@@ -74,7 +74,7 @@ module Mindapp
       %w(call ws redirect invoke email).include? s
     end
     def set_global
-      $xmain= @xmain ; $runseq = @runseq ; $user = current_user ; $xvars= @xmain.xvars; $ip = env["REMOTE_ADDR"]
+      $xmain= @xmain ; $runseq = @runseq ; $user = current_user ; $xvars= @xmain.xvars; $ip = request.env["REMOTE_ADDR"]
     end
     def authorize? # use in pending tasks
       @runseq= @xmain.runseqs.find @xmain.current_runseq
@@ -112,7 +112,7 @@ module Mindapp
     end
     def ma_log(message)
         Mindapp::Notice.create :message => ERB::Util.html_escape(message.gsub("`","'")),
-          :unread=> true, :ip=> ($ip || env["REMOTE_ADDR"])
+          :unread=> true, :ip=> ($ip || request.env["REMOTE_ADDR"])
       # if session[:user_id]
       #   Mindapp::Notice.create :message => ERB::Util.html_escape(message.gsub("`","'")),
       #     :user_id => $user.id, :unread=> true, :ip=> env["REMOTE_ADDR"]
