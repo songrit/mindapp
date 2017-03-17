9abe49e40f954efd71de60ae0148f914d1888f7e
P Kul <1.0@kul.asia>
2017-03-17 17:17:18 +0700

    updated belong_to for Mongoid 6

--- lib/generators/mindapp/install_generator.rb
+++ lib/generators/mindapp/install_generator.rb
@@ -67,6 +67,19 @@ IMAGE_LOCATION = "upload"
 }
         end
 
+initializer "mongoid.rb" do
+%q{# encoding: utf-8
+#  
+# Mongoid 6 follows the new pattern of AR5 requiring a belongs_to relation to always require its parent
+# belongs_to` will now trigger a validation error by default if the association is not present.
+# You can turn this off on a per-association basis with `optional: true`.
+# (Note this new default only applies to new Rails apps that will be generated with
+# `config.active_record.belongs_to_required_by_default = true` in initializer.)  
+#
+Mongoid::Config.belongs_to_required_by_default = false
+}
+        end        
+
         inject_into_file 'config/environment.rb', :after => "initialize!"  do
           "\n\n# hack to fix cloudinary error https://github.com/archiloque/rest-client/issues/141" +
           "\nclass Hash\n  remove_method :read\nrescue\nend"
